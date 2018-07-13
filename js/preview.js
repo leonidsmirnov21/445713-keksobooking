49d153870316ac43aab19156277769c4b4abaedb
Леонид <33185661+leonidsmirnov21@users.noreply.github.com>
2018-07-13 22:25:32 +0300

    rework mistakes

--- js/preview.js
+++ js/preview.js
@@ -4,6 +4,8 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var fileChooserMain = document.querySelector('.ad-form-header__input');
   var previewImg = document.querySelector('.ad-form-header__preview-img');
+  var fileChooserAps = document.querySelector('.ad-form__input');
+  var photosContainer = document.querySelector('.ad-form__photo');
 
   fileChooserMain.addEventListener('change', function () {
     var file = fileChooserMain.files[0];
@@ -23,4 +25,32 @@
       reader.readAsDataURL(file);
     }
   });
+
+  var onFileChooserApsChange = function () {
+    var filesAps = fileChooserAps.files;
+
+    Array.from(filesAps).forEach(function (item) {
+      var fileName = item.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          var imgPhoto = document.createElement('img');
+          imgPhoto.src = reader.result;
+          imgPhoto.style.width = '50%';
+          imgPhoto.style.height = 'auto';
+          photosContainer.appendChild(imgPhoto);
+        });
+
+        reader.readAsDataURL(item);
+      }
+    });
+  };
+
+  fileChooserAps.addEventListener('change', onFileChooserApsChange);
 })();
